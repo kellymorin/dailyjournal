7625b08e5c989721ef0d33fce87eb972e45aad5b
kellymorin <kmorin06@gmail.com>
2018-10-08 11:20:23 -0500

    started on assignment 2 for daily journal

---
+++ scripts/journal.js
@@ -0,0 +1,13 @@
+const journalEntries = [
+  {
+  date: "10/08/2018",
+  concept: "Flexbox",
+  entry: "We learned about using flexbox to style our pages",
+  mood: "fine"
+  }
+]
+
+const makeJournalEntryComponent = (journalEntry) => {
+  // Create HTML structure for a journal entry
+  return
+}
