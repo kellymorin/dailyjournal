8f8ab6fdb04ccb4006cdd01edfc61f3b6cd9dad0
kellymorin <kmorin06@gmail.com>
2018-10-17 11:54:27 -0500

    added journal entries that populate to the DOM with JS.

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,49 +1,34 @@
 const journalEntries = [
-  Entry1, {
+   {
       date: "07/24/2018",
       concept: "Array methods",
       entry: "We learned about 4 different array methods today. forEach made sense, but the others still confuse me.",
       mood: "Ok"
   },
+    {
+  date: "10/08/2018",
+  concept: "Flexbox",
+  entry: "We learned about using flexbox to style our pages",
+  mood: "fine"
+  },
+  {
+    date: "10/17/2018",
+    concept: "Handling data",
+    entry: "We learned about JavaScript to pull important information out of large data files.",
+    mood: "happy",
+  },
 ]
-// const journalEntries = [
-//   {
-//   date: "10/08/2018",
-//   concept: "Flexbox",
-//   entry: "We learned about using flexbox to style our pages",
-//   mood: "fine"
-//   }
-// ]
 
-// Put together a function that takes inputs from the form and changes them into objects and adds them to the journalEntries array
+const entryLog = document.querySelector(".entryLog");
 
-document.getElementById("journalEntryButton").addEventListener("click", function(){
-    console.log("the button is working");
-});
+const makeJournalEntry = (...props)=>{
+  return `<p>Today is: ${props[0]}. We learned about ${props[1]}. ${props[2]}`
+}
 
-let date = document.getElementById("journalDate").value;
-let concept = document.getElementById("conceptsCovered").value;
-let entry = document.getElementById("journalEntry").value;
-let mood = document.getElementById("mood").value;
-console.log("date", date, "concept", concept, "entry", entry, "mood", mood);
+let journalEntry = ""
+journalEntries.forEach((journal)=> {
+  journalEntry = `${makeJournalEntry(journal.date, journal.concept, journal.entry)}`
+  entryLog.innerHTML += journalEntry
+})
 
-// create entry counter to put at the beginning of each entry
-// assign each input to the correct key
-// append each component together 
-// add each object to the array
 
-// const makeJournalEntryComponent = (journalEntry) => {
-//   // Create HTML structure for a journal entry
-//   return `${date}: Today we learned about ${concept}. 
-//   ${entry}`
-// }
-
-// for (i = 0; i<journalEntries.length; i++){
-    // let date = journalEntries.date[i];
-    // let concept = journalEntries.concept[i];
-    // let entry = journalEntries.entry[i];
-    // let mood = journalEntries.mood[i];
-
-    // return `${date}: Today we learned about ${concept}.
-    // ${entry}.`
-// }
